a5d346841fc3e7ad8862f2881c07df71bb1ea430
HishaamDeVries <61458881+HishaamDeVries@users.noreply.github.com>
2020-07-02 15:22:15 +0200

    ready for deploy

---
+++ build/precache-manifest.1404a328abb33ac8364aa6d9381734bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5d31fdcb9e85d00c4921b55302ffeb",
+    "url": "/it-logger/index.html"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/it-logger/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "af159943d8c5dd224525",
+    "url": "/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7cf2e41f3efa52a7b29a",
+    "url": "/it-logger/static/js/2.668ff7c0.chunk.js"
+  },
+  {
+    "revision": "494382bc2e1fa58818bf1f9041d8a9bc",
+    "url": "/it-logger/static/js/2.668ff7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af159943d8c5dd224525",
+    "url": "/it-logger/static/js/main.4e4b752a.chunk.js"
+  },
+  {
+    "revision": "58ab813052182fdf6b64",
+    "url": "/it-logger/static/js/runtime-main.6e8cc9b3.js"
+  }
+]);
