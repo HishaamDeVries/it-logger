7bbe0c0468a5a7265c1a9fe6ae9155401f91942e
HishaamDeVries <61458881+HishaamDeVries@users.noreply.github.com>
2020-07-03 13:42:48 +0200

    deploying it-logger in heroku

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -67,7 +67,8 @@ export default (state = initialState, action) => {
       console.error(action.payload);
       return {
         ...state,
-        error: action.payload,
+        err: action.payload,
+        loading: false,
       };
     default:
       return state;
